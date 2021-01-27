41d4ef9d8580897d5094611ef24788d1c0bf0bd5
Callum <123callumb@gmail.com>
2021-01-27 17:02:14 +0000

    Add project files.

---
+++ Application/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public HomeController() { }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
