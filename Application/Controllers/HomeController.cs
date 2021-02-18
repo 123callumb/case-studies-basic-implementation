a5dc03ea4fe4c86314a913dcdc12dc34a3b4ed5e
Killian <killiancomerford@outlook.com>
2021-02-18 12:39:24 +0400

    Vendor Catalogue Page implementation

--- Application/Controllers/HomeController.cs
+++ Application/Controllers/HomeController.cs
@@ -1,16 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Services.AuthenticationManagement;
+using Services.VendorItemManagement;
 using Services.Filters.Attributes;
 using Services.Models.Enums;
+using Services.Models.DTOs;
+using System.Collections.Generic;
 
 namespace Application.Controllers
 {
     public class HomeController : BaseController
     {
-
-        public HomeController(IAuthenticationManager authManager) : base(authManager) { }
+        private readonly IVendorItemManager _vendorItemManager;
+        public HomeController(IAuthenticationManager authManager, IVendorItemManager vendorItemManager) : base(authManager) {
+            _vendorItemManager = vendorItemManager;
+        }
 
         [RequireUser(UserTypeEnum.INTERNAL)]
         public async Task<IActionResult> Index()
@@ -26,5 +32,24 @@ namespace Application.Controllers
             return View(vm);
         }
 
+        [RequireUser(UserTypeEnum.INTERNAL)]
+        public async Task<IActionResult> VendorCatalogue()
+        {
+            List<VendorItemDTO> vendorItems = await _vendorItemManager.LoadVendorItems();
+            VendorCatalogueViewModel vm = new VendorCatalogueViewModel(await GetSessionUser(), vendorItems);            
+            return View(vm);
+        }
+
+        [RequireUser(UserTypeEnum.INTERNAL)]
+        public async Task<IActionResult> VendorCatalogueSearch(string searchTerm) {
+            ViewBag.SearchTerm = searchTerm;
+            List<VendorItemDTO> vendorItems;
+            
+            if (searchTerm == null) vendorItems = await _vendorItemManager.LoadVendorItems();            
+            else vendorItems = await _vendorItemManager.SearchVendorItems(searchTerm);
+
+            VendorCatalogueViewModel vm = new VendorCatalogueViewModel(await GetSessionUser(), vendorItems);
+            return View("VendorCatalogue", vm);
+        }
     }
 }
