4a4e376b110092422e8d0393727efaf26ad50ed3
Callum <123callumb@gmail.com>
2021-02-25 20:08:01 +0000

    authentication tests

--- UnitTests/ManagerTests/AuthenticationManagerTests.cs
+++ UnitTests/ManagerTests/AuthenticationManagerTests.cs
@@ -6,10 +6,12 @@ using Services.AuthenticationManagement.Models;
 using Services.EntityFramework.DbEntities;
 using Services.Models.Abstract;
 using Services.Models.DTOs;
+using Services.Models.Enums;
 using Services.SessionManagement.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace UnitTests.ManagerTests
@@ -88,14 +90,86 @@ namespace UnitTests.ManagerTests
             var c = new MockContainer();
             var session = new Mock<ISession>();
             var manager = Manager(c);
-
-            session.Setup(s => s.GetUserSession()).Returns(null as AuthenticatedSession);
+            byte[] userSession = { };
+            session.Setup(s => s.TryGetValue(SessionHelper.UserSessionKey, out userSession)).Returns(false);
 
             // Act
             var res = await manager.GetSessionUser(session.Object);
 
             // Assert
             Assert.IsNull(res);
+        }
+
+        [TestMethod]
+        public async Task GetSessionUser_AnyUserType_ReturnsNull()
+        {
+            // Arrange
+            var c = new MockContainer();
+            var session = new Mock<ISession>();
+            var sessionObj = session.Object;
+            var manager = Manager(c);
+            int userID = 290;
+            byte[] userSession = JsonSerializer.SerializeToUtf8Bytes(new AuthenticatedSession(userID, UserTypeEnum.ANY));
+            session.Setup(s => s.TryGetValue(SessionHelper.UserSessionKey, out userSession)).Returns(true);
+            
+            // Act
+            var res = await manager.GetSessionUser(sessionObj);
+
+            // Assert
+            Assert.IsNull(res);
+        }
+
+        [TestMethod]
+        public async Task GetSessionUser_InternalUser_Success()
+        {
+            // Arrange
+            var c = new MockContainer();
+            var session = new Mock<ISession>();
+            var sessionObj = session.Object;
+            var manager = Manager(c);
+            int userID = 290;
+            byte[] userSession = JsonSerializer.SerializeToUtf8Bytes(new AuthenticatedSession(userID, UserTypeEnum.INTERNAL));
+            session.Setup(s => s.TryGetValue(SessionHelper.UserSessionKey, out userSession)).Returns(true);
+            c.GenericQuerier.Setup(s => s.Load(It.IsAny<Expression<Func<User, InternalUserDTO>>>(), It.IsAny<Expression<Func<User, bool>>>())).Returns(new List<InternalUserDTO>()
+            {
+                new InternalUserDTO()
+                {
+                    UserType = UserTypeEnum.INTERNAL,
+                    UserID = userID
+                }
+            }.GetMockQueryable());
+            // Act
+            var res = await manager.GetSessionUser(sessionObj);
+
+            // Assert
+            Assert.AreEqual(res.UserType, UserTypeEnum.INTERNAL);
+        }
+
+        [TestMethod]
+        public async Task GetSessionUser_ExternalUser_Success()
+        {
+            // Arrange
+            var c = new MockContainer();
+            var session = new Mock<ISession>();
+            var sessionObj = session.Object;
+            var manager = Manager(c);
+            int userID = 290;
+            byte[] userSession = JsonSerializer.SerializeToUtf8Bytes(new AuthenticatedSession(userID, UserTypeEnum.EXTERNAL));
+            session.Setup(s => s.TryGetValue(SessionHelper.UserSessionKey, out userSession)).Returns(true);
+            c.GenericQuerier.Setup(s => s.Load(It.IsAny<Expression<Func<VendorUser, ExternalUserDTO>>>(), It.IsAny<Expression<Func<VendorUser, bool>>>())).Returns(new List<ExternalUserDTO>()
+            {
+                new ExternalUserDTO()
+                {
+                    UserType = UserTypeEnum.EXTERNAL,
+                    UserID = userID
+                }
+            }.GetMockQueryable());
+
+            // Act
+            var res = await manager.GetSessionUser(sessionObj);
+
+            // Assert
+            Assert.AreEqual(res.UserType, UserTypeEnum.EXTERNAL);
         }
     }
 }
