bad9bc4a7a25c256bb25d46fd5461f83fc095926
Callum <123callumb@gmail.com>
2021-02-25 09:42:15 +0000

    wip not working yet

---
+++ UnitTests/ManagerTests/QuoteManagerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.QuoteManagement.Implementation
+
+namespace UnitTests.ManagerTests
+{
+    [TestClass]
+    public class QuoteManagerTests
+    {
+        private QuoteManager Manager()
+        {
+            return new QuoteManager();
+        }
+
+        [TestMethod]
+        public async Task GetQuote_Success()
+        {
+            QuoteManager quoteManager = new QuoteManager();
+        }
+    }
+}
