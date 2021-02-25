093230cb12558a0d5a49a00c6d1b0975d7da0885
Harrison Fenton-Fearn <harryff007@gmail.com>
2021-02-25 08:42:55 +0000

    Quote page displays all quotes in date order

--- Services/QuoteManagement/IQuoteManager.cs
+++ Services/QuoteManagement/IQuoteManager.cs
@@ -6,7 +6,8 @@ namespace Services.QuoteManagement
 {
     public interface IQuoteManager
     {
-        Task<bool> RequestQuote(VendorItemDTO item, int quantity); 
+        Task<bool> RequestQuote(VendorItemDTO item, int quantity);
+        Task<List<QuoteOverviewDTO>> GetVendorQuotes();
         Task<List<QuoteOverviewDTO>> GetVendorQuotes(int vendorID);
         Task<QuoteDTO> GetQuote(int quoteID);
     }
