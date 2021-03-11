a87d9f66cb7d2e3b40a7b389ed5f30535663812f
Harrison Fenton-Fearn <harryff007@gmail.com>
2021-03-11 09:26:56 +0000

    Moved the audio controls to the top of the page Added some comments to the code

--- Application/wwwroot/lib/talkify/audioReader.js
+++ Application/wwwroot/lib/talkify/audioReader.js
@@ -1,17 +1,27 @@
-﻿talkify.config.remoteService.host = 'https://talkify.net';
+﻿
+/*
+ * Talkify.js allows us to send a request for an text to speech audio sample that can
+ * be played on the website. Here we have set up the talkify apiKey required for the service.
+ * You are allocated 1000 free requests a month on the free account. This is enought for
+ * testing purposes.
+ */
+
+talkify.config.remoteService.host = 'https://talkify.net';
 talkify.config.remoteService.apiKey = '298c9307-7ac8-4654-8cf5-7de63ff4f95f';
 
+// Setting up the audio controls for the webpage to allow the user to pause or replay the sounds
 talkify.config.ui.audioControls.enabled = true;
 talkify.config.ui.audioControls.voicepicker.enabled = true;
 talkify.config.ui.audioControls.container = document.getElementById("player-and-voices");
 
-
+// Configure the audio player with the voice of Hazel (GB).
 var player = new talkify.TtsPlayer()
     .enableTextHighlighting()
     .forceVoice({ name: "Hazel" });
 
-
 var btnAudioAssist = document.querySelector('#audioAssistButton');
+
+// Add the click event listener to the audio assist button and make the button toggelable
 btnAudioAssist.addEventListener('click',
     (event) => {
         if (btnAudioAssist.classList.contains('active')) {
@@ -24,6 +34,9 @@ btnAudioAssist.addEventListener('click',
         }
     });
 
+// Find each element with the data-voice attribute and add a mouse over event listener to
+// use the audio player to play the inner Text of the element or the specified msg written in
+// the data-voice attribute. This is used for custom messages for specific elements.
 document.querySelectorAll('[data-voice]').forEach(item => {
     item.addEventListener('mouseover',
         (event) => {
